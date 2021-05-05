0161cd8fac5e86075954cfcd894cb55a0c562bee
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-05-05 16:18:29 -0700

    First two tests complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,8 +7,12 @@ const request = supertest(app);
 
 describe('API Routes', () => {
 
+  // beforeAll(() => {
+  //   execSync('npm run setup-db');
+  // });
+
   beforeAll(() => {
-    execSync('npm run setup-db');
+    execSync('npm run recreate-tables');
   });
 
   afterAll(async () => {
@@ -60,7 +64,7 @@ describe('API Routes', () => {
   // If a GET request is made to /api/items, does:
   // 1) the server respond with status of 200
   // 2) the body match the expected API data?
-  it('GET /api/items', async () => {
+  it.skip('GET /api/items', async () => {
     // act - make the request
     const response = await request.get('/api/items');
 
@@ -75,9 +79,58 @@ describe('API Routes', () => {
   // If a GET request is made to /api/cats/:id, does:
   // 1) the server respond with status of 200
   // 2) the body match the expected API data for the cat with that id?
-  test('GET /api/items/:id', async () => {
+  test.skip('GET /api/items/:id', async () => {
     const response = await request.get('/api/items/2');
     expect(response.status).toBe(200);
     expect(response.body).toEqual(expectedItems[1]);
+  });
+
+  let health = {
+    id: expect.any(Number),
+    name: 'Amulet of Health',
+    description: 'Your Constitution score is 19 while you wear this amulet. It has no effect on you if your Constitution is already 19 or higher.',
+    type: 'Wondrous item',
+    rarity: 'rare',
+    requiresAttunement: true
+  };
+
+  let planes = {
+    id: expect.any(Number),
+    name: 'Amulet of the Planes',
+    description: 'While wearing this amulet, you can use an action toname a location that you are familiar with on another plane of existence. Then make a DC 15 Intelligence check. On a successful check, you cast the _plane shift_ spell. On a failure, you and each creature and object within 15 feet of you travel to a random destination. Roll a d100. On a 1-60, you travel to a random location on the plane youname . On a 61-100, you travel to a randomly determined plane of existence.',
+    type: 'Wondrous iem', 'rarity': 'very rare',
+    requiresAttunement: true
+  };
+
+  let shield = {
+    id: expect.any(Number),
+    name: 'Animated Shield',
+    description: 'While holding this shield, you can speak its command word as a bonus action to cause it to animate. The shield leaps into the air and hovers in your space to protect you as if you were wielding it, leaving your hands free. The shield remains animated for 1 minute, until you use a bonus action to end this effect, or until you are incapacitated or die, at which point the shield falls to the ground or into your hand if you have one free.',
+    type: 'Armor (shield)',
+    rarity: 'very rare',
+    requiresAttunement: true
+  };
+
+  it('POST health to /api/items', async () => {
+    const response = await request
+      .post('/api/items')
+      .send(health);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(health);
+
+    health = response.body;
+  });
+
+  it('PUT updated shield to /api/items/:id', async () => {
+    health.name = 'Suberb Amulet of Health';
+    health.rarity = 'legendary';
+
+    const response = await request
+      .put(`/api/items/${health.id}`)
+      .send(health);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(health);
   });
 });
